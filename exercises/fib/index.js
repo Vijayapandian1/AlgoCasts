9d2be821b49bd04e9e06b7b338da8488fe0ffe1c
vijayapandian <vijayapandian1@gmail.com>
2020-12-13 18:01:40 -0500

    checkin

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,59 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//My Solution
+/*
+function fib(n) {
+    var previous = 1;
+    var previousOfPRevious = 1;
+    var current = 0;
+    for(var i=0; i <= n; i++){
+      if(i < 2){
+          current = i;
+      }
+      else if(i == 2){
+      	current = 1;
+      }
+      else{
+          var current = previousOfPRevious + previous;
+          previousOfPRevious = previous;
+          previous = current;
+      }
+    }
+    return current;
+}
+*/
+//Solution 1
+/*
+function fib(n) {
+        let array = [0,1];
+
+        for(let i=2; i<=n;i++){
+            array.push(array[array.length-1]+array[array.length-2]);
+        }
+
+        return array[n];
+}
+*/
+// My Solution Recursive
+/*
+function fib(n, array = [0,1]){
+    if(array.length - 1 === n || n < 2){
+        return array[n];
+    }
+
+    let r = array[array.length-1]+array[array.length-2];
+    array.push(r);
+    return fib(n,array);
+
+}
+*/
+//Solution 2    Recursive
+function fib(n){
+    if(n < 2)
+    return n;
+
+    return fib(n - 1) + fib(n -2);
+}
 
 module.exports = fib;
