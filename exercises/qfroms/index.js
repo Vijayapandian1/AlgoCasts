bc9d311c8af924d4dc6f14fc59dda19c4f509c5e
vijayapandian <vijayapandian1@gmail.com>
2020-12-17 01:07:05 -0500

    all commits

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,39 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor(){
+        this.s1 = new Stack();
+        this.s2 = new Stack();
+    }
+
+    add(record){
+        return this.s1.push(record);
+        // while(this.s1.peek()){
+        //     this.s2.push(this.s1.pop());
+        // }
+    }
+
+    remove(){
+        while(this.s1.peek()){
+            this.s2.push(this.s1.pop());
+        }
+        let a = this.s2.pop();
+        while(this.s2.peek()){
+            this.s1.push(this.s2.pop());
+        }
+        return a;
+    }
+    peek(){
+        while(this.s1.peek()){
+            this.s2.push(this.s1.pop());
+        }
+        let a = this.s2.peek();
+        while(this.s2.peek()){
+            this.s1.push(this.s2.pop());
+        }
+        return a;
+    }
+}
 
 module.exports = Queue;
